bfb73d84c598429b481f7f0f4b428f22ceaf317d
kevin-shitaye <it.kevin.shitaye@gmail.com>
2022-01-23 16:24:51 +0300

    home page content customization part 1

--- header.php
+++ header.php
@@ -18,7 +18,15 @@
 </header>
 
 <?php get_template_part('template-parts/navigation/navigation', 'primary'); ?>
-<?php get_template_part('template-parts/header/header', 'image'); ?>
+
+
+<?php 
+    if (is_front_page()) {
+        get_template_part('template-parts/header/header', 'image');
+        get_template_part('template-parts/header/header', 'callToAction');
+    }
+ 
+?>
 
 <!-- <h1>   <a href="<?php echo home_url()?>"><?php bloginfo('name')?></a>  </h1>
 <h4><?php bloginfo('description')?></h4> -->
