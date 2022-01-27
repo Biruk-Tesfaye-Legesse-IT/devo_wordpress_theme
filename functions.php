86585f466f1b18d1aca2e5283ab93aa32c8b3627
kevin-shitaye <it.kevin.shitaye@gmail.com>
2022-01-27 10:03:57 +0300

    create home and contact but template is not working

--- functions.php
+++ functions.php
@@ -17,6 +17,39 @@ require get_template_directory().'/inc/front-about.php';
 // ========================Adding services customizer=======================================================
 
 require get_template_directory().'/inc/service-customizer.php';
- 
+
+
+
+// creating default home page
+if(get_page_by_title("Home") == null)
+{
+    $post = array(
+        "post_title" => "Home",
+        "post_status" => "publish",
+        "post_type" => "page",
+        "menu_order" => "-100",
+        "page_template" => "front-default.php"
+    );
+
+    wp_insert_post($post);
+
+    $home_page = get_page_by_title("Home");
+    update_option("page_on_front",$home_page->ID);
+    update_option("show_on_front","page");
+
+}
+if(get_page_by_title("Contact") == null)
+{
+    $post = array(
+        "post_title" => "Contact",
+        "post_status" => "publish",
+        "post_type" => "page",
+        "menu_order" => "-100",
+        "page_template" => "single-contact.php"
+    );
+
+    wp_insert_post($post);
+
+}
 
 ?>
